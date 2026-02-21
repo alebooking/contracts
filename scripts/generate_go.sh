f97e0b729ef9838117e5d743e8d738ac9ce61364
Alebro112 <sevas.bl@gmail.com>
2026-02-22 00:34:52 +0200

    fix: generate_go.sh syntax

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -7,7 +7,7 @@ protoc -I ./proto \
     --go_out=./gen/go \
     --go-grpc_out=./gen/go \
     --go_opt=module=github.com/alebooking/contracts \
-    --go-rpc_opt=module=github.com/alebooking/contracts \
+    --go-grpc_opt=module=github.com/alebooking/contracts \
     ./proto/media.proto
 
 ecoh "Go generation complete."
